288753c61bb44061b2d3a371ed9e523baa413991
Diana Clabel <dianaclabel08@gmail.com>
2023-03-10 00:34:49 -0500

    add structure HTML to container-pay

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 console.log(validator);
+
+const comprarBtn = document.querySelector("#btn-comprar");
+
+comprarBtn.addEventListener("click", () => {});
