9d0a9a0b8ec4f9b0e3c6d986dc1c8ca587801dd0
Diana Clabel <dianaclabel08@gmail.com>
2023-03-22 16:48:52 -0500

    add footer to home

--- src/index.js
+++ src/index.js
@@ -1,16 +1,57 @@
 import validator from "./validator.js";
 
-const cardNumberInput = document.querySelector("#input-card-number");
-const nameLastnameInput = document.querySelector("#name-lastname");
-// const cardForm = document.querySelector("#card-form");
+const cardForm = document.querySelector("#card-form");
 // const formBtn = document.querySelector("#form-btn");
+
+const cardNumberInput = document.querySelector("#input-card-number");
+const nameLastnameInput = document.querySelector("#input-name-lastname");
+const expirationDateInput = document.querySelector("#input-expiration-date");
+const checkCardDiv = document.querySelector("#check-card");
+const checkCardImg = document.querySelector("#img-check-card");
 
 cardNumberInput.addEventListener("keyup", () => {
   const cardNumbersString = cardNumberInput.value;
-  // console.log(cardNumbersString);
+  const masked = validator.maskify(cardNumbersString);
   const isvalid = validator.isValid(cardNumbersString);
-  const maskify = validator.maskify(cardNumbersString);
 
-  const cardCreditNumberInput = document.querySelector("#card-credit-number");
-  cardCreditNumberInput.innerHTML = maskify.replaceAll("#", "*");
+  const cardCreditNumberP = document.querySelector("#card-credit-number");
+  cardCreditNumberP.innerHTML = masked
+    .replaceAll("#", "*")
+    .split(/(.{4})/)
+    .filter((s) => s.length)
+    .join(" ");
+
+  if (cardNumbersString.length >= 15) {
+    checkCardDiv.style.display = "block";
+    if (isvalid) {
+      checkCardImg.src = "./assets/check.png";
+    } else {
+      checkCardImg.src = "./assets/error.png";
+    }
+  } else {
+    checkCardDiv.style.display = "none";
+  }
+});
+
+nameLastnameInput.addEventListener("keyup", () => {
+  const valueNameLastname = nameLastnameInput.value;
+  const nameLastnameP = document.querySelector("#name-lastname");
+  nameLastnameP.innerHTML = valueNameLastname.toUpperCase();
+});
+
+expirationDateInput.addEventListener("keyup", () => {
+  const valueExpirationDate = expirationDateInput.value;
+  const expirationDateP = document.querySelector("#expiration-date");
+  expirationDateP.innerHTML = valueExpirationDate;
+});
+
+cardForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  //formData es una instancia para captar los datos
+  const datos = new FormData(cardForm);
+
+  const nombreUsuario = datos.get("card-name");
+
+  alert(nombreUsuario + ", gracias por su compra");
 });
